ed86b3a19ac2d3cf9700a9abe4a82541b2f22e9f
ssaitou126 <ssaitou126@gmail.com>
2023-01-18 11:21:34 +0900

    Link_Test

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./templates/images')
 
-@app.route("/")
-def hello_world():
-   return "<p>Hello, World! from saitou こんにちわ</p>"
+a=30
+b=4
+
+@app.route('/')
+def index():
+    city = '滝沢'
+    calc=a+b
+    return render_template('index.html',name="斉藤さん",city=city,calc=calc)
+
+@app.route('/sub1')
+def sub1():
+    return render_template('sub1.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
